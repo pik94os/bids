26b46e0d838c9e2d034d67a23db50cbaec107f3f
Roman Shulgin <pik94os.po@gmail.com>
2016-05-18 18:42:09 +0300

    допилы аукционов

--- front/javascripts/controllers/Auction.js
+++ front/javascripts/controllers/Auction.js
@@ -4,5 +4,12 @@ define(['./module','jquery'],function(controllers,$){
         $scope.open = $stateParams.open;
     }]).controller('Auction',['$scope','$http', '$rootScope', '$stateParams', function($scope,$http,$rootScope,$stateParams){
         $scope.open = $stateParams.open;
+        $scope.contactsShow= false;
+        $scope.showContacts = function () {
+            $scope.contactsShow= true;
+        }
+        $scope.hideContacts = function () {
+            $scope.contactsShow= false;
+        }
     }])
 });
