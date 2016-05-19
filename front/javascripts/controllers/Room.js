aafb9fd50e5a2303a2cf01e0c28779fd07ac03f8
piligrim <piligrim18@gmail.com>
2016-05-19 18:46:44 +0300

    Адаптивная верстка room

--- front/javascripts/controllers/Room.js
+++ front/javascripts/controllers/Room.js
@@ -1,6 +1,8 @@
 define(['./module','jquery'],function(controllers,$){
     'use strict';
-    controllers.controller('Room',['$scope','$http', '$rootScope', '$stateParams', function($scope,$http,$rootScope,$stateParams){
+    controllers.controller('RoomHeader',['$scope','$http', '$rootScope', '$stateParams', function($scope,$http,$rootScope,$stateParams){
+        $scope.countdown = $stateParams.countdown;
+    }]).controller('Room',['$scope','$http', '$rootScope', '$stateParams', function($scope,$http,$rootScope,$stateParams){
         $scope.countdown = $stateParams.countdown;
     }])
 });
