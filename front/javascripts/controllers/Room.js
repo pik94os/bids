633007db5a83af6b42f5a1b4070f126882f36602
Roman Shulgin <pik94os.po@gmail.com>
2016-05-25 16:57:04 +0300

    адаптивные чупачупсы

--- front/javascripts/controllers/Room.js
+++ front/javascripts/controllers/Room.js
@@ -7,6 +7,32 @@ define(['./module','jquery'],function(controllers,$){
         $scope.popup = false;
         $scope.setPopup = function(index){
             $scope.popup = index;
+        };
+        function moveToTheRigh() {
+            var countInFirstLine = 0;
+            var lt = $('.lot-table:first-child');
+            var childNum = 1;
+            var i=2;
+            $('.lot-table.moveToTheRight').removeClass('moveToTheRight');
+            while(childNum && lt.length){
+                lt = lt.next();
+                if(lt.position().top === $('.lot-table:first-child').position().top){
+                    countInFirstLine++
+                }else{
+                    lt = $('.lot-table:nth-child('+((countInFirstLine+2)*childNum - (2*childNum-(i++)))+')').addClass('moveToTheRight');
+                    if(lt.length){
+                        childNum+=2;
+                    }else{
+                        childNum = 0;
+                    }
+                }
+            }
         }
+        $scope.$on('LastRepeaterElement', function(){
+            moveToTheRigh();
+        });
+        $(window).resize( function(){
+            moveToTheRigh();
+        });
     }])
 });
