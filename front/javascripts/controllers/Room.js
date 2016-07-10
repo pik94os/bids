c7422ef98db0916ba4e73b931a31ab4e8f9afbd9
pik <pik94os.po@gmail.com>
2016-07-10 19:10:04 +0300

    правки с выводом времени и номера аукциона

--- front/javascripts/controllers/Room.js
+++ front/javascripts/controllers/Room.js
@@ -1,9 +1,9 @@
 define(['./module','jquery'],function(controllers,$){
     'use strict';
     controllers.controller('RoomHeader',['$scope','$http', '$rootScope', '$stateParams', function($scope,$http,$rootScope,$stateParams){
-        $scope.countdown = $stateParams.countdown;
+        $scope.countdown = +$stateParams.countdown-1?2:1;
     }]).controller('Room',['$scope','$http', '$rootScope', '$stateParams', function($scope,$http,$rootScope,$stateParams){
-        $scope.countdown = $stateParams.countdown;
+        $scope.countdown = +$stateParams.countdown-1?2:1;
         $scope.popup = false;
         $scope.setPopup = function(index){
             $scope.popup = index;
