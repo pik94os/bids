21e1276021d4275a89de0b9989f32c570b4cce17
pik <pik94os.po@gmail.com>
2016-07-15 02:14:00 +0300

    зародыш начала аукциона

--- app/sockets/auction/startAuction.js
+++ app/sockets/auction/startAuction.js
@@ -9,19 +9,20 @@ const Lot = require('../../models').Lot;
 module.exports = function (socket, data) {
     Lot.findOne({
         where: {id: data.id}
-    }).then((result)=> {
-        result.isPlayOut = true;
-        return result.save().then(()=>{
-            socket.emit('auctionStart',{
-                err: 0,
-                data: result
+    }).then((lot)=> {
+        return Auction.update({
+            start: new Date
+        }, {
+            where: {id: data.id}
+        }).then(()=> {
+            lot.isPlayOut = true;
+            return lot.save().then(()=> {
+                socket.emit('auctionStart', {
+                    err: 0,
+                    data: lot
+                });
             });
-        }).catch((err)=>{
-            socket.emit('auctionStart',{
-                err: 1,
-                message: err.message
-            });
-        })
+        });
     }).catch((err)=>{
         socket.emit('auctionStart',{
             err: 1,
