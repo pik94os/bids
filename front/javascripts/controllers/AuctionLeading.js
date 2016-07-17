bd6b1e08d35b0b9038325df29509ce010f53d088
andrey <aanndd_g@mail.ru>
2016-07-17 14:40:12 +0300

    В розовом поле Следуйющий бид – нужно показывать следующий ШАГ за который выставляется ЛОТ.

--- front/javascripts/controllers/AuctionLeading.js
+++ front/javascripts/controllers/AuctionLeading.js
@@ -165,6 +165,7 @@ define(['./module','jquery'],function(controllers,$){
         ngSocket.on('lotConfirmed', function (data) {
             ngSocket.emit('auction/getListBids', {auctionId: $stateParams.auctionId});
             $scope.price = data.bid.price;
+            $scope.priceNext = $scope.price + calcStep(data.bid.price);
             $scope.userNumber = data.bid.userId;
             $scope.userData = data.userName.firstName + ' ' + data.userName.lastName + ' ' + data.userName.patronymic;
         });
@@ -197,49 +198,54 @@ define(['./module','jquery'],function(controllers,$){
         });
 
 
-        // вычисление шага для цены продажи (не доделано)
-        // function calcStep(price){
-        //     var step = 1;
-        //     if (price <= 5){
-        //         step = 1;
-        //     }
-        //     if (5 < price &&  price <= 50){
-        //         step = 10;
-        //     }
-        //     if (50 < price && price <= 200){
-        //         step = 20;
-        //     }
-        //     if (200 < price && price <= 500){
-        //         step = 50;
-        //     }
-        //     if (500 < price && price <= 1000){
-        //         step = 100;
-        //     }
-        //     if (2000 < price && price <= 5000){
-        //         step = 500;
-        //     }
-        //     if (5000 < price && price <= 10000){
-        //         step = 1000;
-        //     }
-        //     if (10000 < price && price <= 20000){
-        //         step = 2000;
-        //     }
-        //     if (20000 < price && price <= 50000){
-        //         step = 5000;
-        //     }
-        //     if (50000 < price && price <= 100000){
-        //         step = 10000;
-        //     }
-        //     if (100000 < price && price <= 200000){
-        //         step = 20000;
-        //     }
-        //     if (200000 < price && price <= 500000){
-        //         step = 50000;
-        //     }
-        //     if (500000 < price && price <= 1000000){
-        //         step = 100000;
-        //     }
-        //     return step;
-        // }
+        //вычисление шага для цены продажи (не доделано ??)
+        function calcStep(price){
+            var step = 1;
+            if (price <= 5){
+                return step = 1;
+            }
+            if (5 < price && price <= 50) {
+                return step = 5;
+            }
+            if (50 < price && price <= 200){
+                return step = 10;
+            }
+            if (200 < price && price <= 500){
+                return step = 20;
+            }
+            if (500 < price && price <= 1000){
+                return step = 50;
+            }
+            if (1000 < price && price <= 2000) {
+                return step = 100;
+            }
+            if (2000 < price && price <= 5000){
+                return step = 200;
+            }
+            if (5000 < price && price <= 10000){
+                return step = 500;
+            }
+            if (10000 < price && price <= 20000){
+                return step = 1000;
+            }
+            if (20000 < price && price <= 50000){
+                return step = 2000;
+            }
+            if (50000 < price && price <= 100000){
+                return step = 5000;
+            }
+            if (100000 < price && price <= 200000){
+                return step = 10000;
+            }
+            if (200000 < price && price <= 500000){
+                return step = 20000;
+            }
+            if (500000 < price && price <= 1000000){
+                return step = 50000;
+            } else {
+                step = 10000;
+            }
+            return step;
+        }
     }])
 });
