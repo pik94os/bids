bad00193a0a7194d3d40796714f3ad644c55bb86
pik <pik94os.po@gmail.com>
2016-07-21 11:03:01 +0300

    фикс ставок

--- front/javascripts/controllers/Room.js
+++ front/javascripts/controllers/Room.js
@@ -624,52 +624,52 @@ define(['./module','jquery'],function(controllers,$){
                 });
                 return obj;
             }
-            function calcStep(price){
-                var step = 1;
-                if (price <= 5){
-                    return step = 1;
-                }
-                if (5 < price && price <= 50) {
-                    return step = 5;
-                }
-                if (50 < price && price <= 200){
-                    return step = 10;
-                }
-                if (200 < price && price <= 500){
-                    return step = 20;
-                }
-                if (500 < price && price <= 1000){
-                    return step = 50;
-                }
-                if (1000 < price && price <= 2000) {
-                    return step = 100;
-                }
-                if (2000 < price && price <= 5000){
-                    return step = 200;
-                }
-                if (5000 < price && price <= 10000){
-                    return step = 500;
-                }
-                if (10000 < price && price <= 20000){
-                    return step = 1000;
-                }
-                if (20000 < price && price <= 50000){
-                    return step = 2000;
-                }
-                if (50000 < price && price <= 100000){
-                    return step = 5000;
-                }
-                if (100000 < price && price <= 200000){
-                    return step = 10000;
-                }
-                if (200000 < price && price <= 500000){
-                    return step = 20000;
-                }
-                if (500000 < price && price <= 1000000){
-                    return step = 50000;
-                } else {
-                    step = 100000;
-                }
-                return step;
-            }
+    function calcStep(price) {
+        var step = 1;
+        if (price <= 5) {
+            return step = 1;
+        }
+        if (5 < price && price < 50) {
+            return step = 5;
+        }
+        if (50 <= price && price < 200) {
+            return step = 10;
+        }
+        if (200 < price && price < 500) {
+            return step = 20;
+        }
+        if (500 <= price && price < 1000) {
+            return step = 50;
+        }
+        if (1000 <= price && price < 2000) {
+            return step = 100;
+        }
+        if (2000 <= price && price < 5000) {
+            return step = 200;
+        }
+        if (5000 <= price && price < 10000) {
+            return step = 500;
+        }
+        if (10000 <= price && price < 20000) {
+            return step = 1000;
+        }
+        if (20000 <= price && price < 50000) {
+            return step = 2000;
+        }
+        if (50000 <= price && price < 100000) {
+            return step = 5000;
+        }
+        if (100000 <= price && price < 200000) {
+            return step = 10000;
+        }
+        if (200000 <= price && price < 500000) {
+            return step = 20000;
+        }
+        if (500000 <= price && price < 1000000) {
+            return step = 50000;
+        } else {
+            step = 100000;
+        }
+        return step;
+    }
 });
