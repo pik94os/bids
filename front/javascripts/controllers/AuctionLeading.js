b69e424cd40f11a0bf49b8ee30f8ee815a3d3ac1
pik <pik94os.po@gmail.com>
2016-07-21 10:59:43 +0300

    fix видео

--- front/javascripts/controllers/AuctionLeading.js
+++ front/javascripts/controllers/AuctionLeading.js
@@ -334,43 +334,43 @@ define(['./module', 'jquery'], function (controllers, $) {
             if (price <= 5) {
                 return step = 1;
             }
-            if (5 < price && price <= 50) {
+            if (5 < price && price < 50) {
                 return step = 5;
             }
-            if (50 < price && price <= 200) {
+            if (50 <= price && price < 200) {
                 return step = 10;
             }
-            if (200 < price && price <= 500) {
+            if (200 < price && price < 500) {
                 return step = 20;
             }
-            if (500 < price && price <= 1000) {
+            if (500 <= price && price < 1000) {
                 return step = 50;
             }
-            if (1000 < price && price <= 2000) {
+            if (1000 <= price && price < 2000) {
                 return step = 100;
             }
-            if (2000 < price && price <= 5000) {
+            if (2000 <= price && price < 5000) {
                 return step = 200;
             }
-            if (5000 < price && price <= 10000) {
+            if (5000 <= price && price < 10000) {
                 return step = 500;
             }
-            if (10000 < price && price <= 20000) {
+            if (10000 <= price && price < 20000) {
                 return step = 1000;
             }
-            if (20000 < price && price <= 50000) {
+            if (20000 <= price && price < 50000) {
                 return step = 2000;
             }
-            if (50000 < price && price <= 100000) {
+            if (50000 <= price && price < 100000) {
                 return step = 5000;
             }
-            if (100000 < price && price <= 200000) {
+            if (100000 <= price && price < 200000) {
                 return step = 10000;
             }
-            if (200000 < price && price <= 500000) {
+            if (200000 <= price && price < 500000) {
                 return step = 20000;
             }
-            if (500000 < price && price <= 1000000) {
+            if (500000 <= price && price < 1000000) {
                 return step = 50000;
             } else {
                 step = 100000;
