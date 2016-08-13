bd691a44a2f357ab84132a2866de1846d1c49ef4
pik <pik94os.po@gmail.com>
2016-08-13 14:12:13 +0300

    обязал safari использовать flash, или canvas

--- front/javascripts/controllers/Room.js
+++ front/javascripts/controllers/Room.js
@@ -721,7 +721,19 @@ define(['./module','jquery'],function(controllers,$){
                 port = "8443";
             }
             url = proto + ":" + port;
-            if(supports_video()){
+            var ua = navigator.userAgent;
+            var bName = function () {
+                if (ua.search(/MSIE/) > -1) return "ie";
+                if (ua.search(/Firefox/) > -1) return "firefox";
+                if (ua.search(/Opera/) > -1) return "opera";
+                if (ua.search(/Chrome/) > -1) return "chrome";
+                if (ua.search(/Safari/) > -1) return "safari";
+                if (ua.search(/Konqueror/) > -1) return "konqueror";
+                if (ua.search(/Iceweasel/) > -1) return "iceweasel";
+                if (ua.search(/SeaMonkey/) > -1) return "seamonkey";
+            }();
+
+            if(supports_video() && bName!=="safari"){
                 initVideoRTC(url);
             }else if(supports_flash()){
                 $scope.updateBrowser = true;
